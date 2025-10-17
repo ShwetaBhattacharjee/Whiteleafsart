7bf0d723b198e9c27ae59dec17c6979fe9e89368
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-10-17 15:47:43 +0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['example.com'], // Replace with actual domains hosting your images (e.g., your CDN or storage service)
-    },
-    eslint: {
-        // Optional: Ignore ESLint during builds (use only if you can't fix errors immediately)
-        // ignoreDuringBuilds: true,
-    },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
